4fbfb8a98abca8ce5bac7607ffc08d472a18a767
lietryon <29160922+lietryon@users.noreply.github.com>
2020-09-09 18:04:37 +0200

    Added movement controls
    - Wizard can now move to the either side with the sprite flipping properly.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the character
+        transform.Translate(Input.GetAxis("Horizontal")* 15f * Time.deltaTime, 0f, 0f);
+
+        // Flip the character
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -1;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 1;
+        }
+        transform.localScale = characterScale;
+        
+    }
+}
